c0351af469b7b8c45701369116e9ee3d15a0ed54
bjulien <julien.brousse@ynov.com>
2024-10-21 11:33:11 +0200

    Add all

---
+++ app/register.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/register.css">
+    <title>Register</title>
+</head>
+<body>
+<div class="register">
+    <h1>Register</h1>
+    <form action="authenticate.php" method="post">
+        <label for="username">Username:</label> 
+        <input id="username" name="username" required="" type="text" />
+        <label for="email">Email:</label>
+        <input id="email" name="email" required="" type="email" />
+        <label for="password">Password:</label>
+        <input id="password" name="password" required="" type="password" />
+        <input name="register" type="submit" value="Register" />
+    </form>
+</div>
+</body>
+</html>
