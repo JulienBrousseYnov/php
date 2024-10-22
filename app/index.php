5fd6da38c65cdb6c8475b73a68c209d29900ccb4
bjulien <julien.brousse@ynov.com>
2024-10-22 11:11:39 +0200

    Change background color, button, and text form de index page

--- app/index.php
+++ app/index.php
@@ -14,7 +14,7 @@ include 'informations.php';
     <meta charset="UTF-8">
 
     <meta name="viewport" content="width=device-width, target-densitydpi=device-dpi"/>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css\style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&display=swap" rel="stylesheet">
@@ -30,15 +30,15 @@ include 'informations.php';
 
     <!-- TITLE SECTION -->
     <div class="title">
-        <h1>WELCOME HERE</h1>
-        <p>Curriculum vitæ</p>
+        <h1>Bienvenue sur mon site CV</h1>
+        <p>Un site internet CV est une plateforme personnelle en ligne où un individu présente ses compétences, expériences professionnelles, et réalisations sous forme de CV numérique. Vous y retrouvez mon CV ainsi que beaucoup d'autres</p>
     </div>
 
 
     <!-- BUTTON SECTION -->
 <div class="cv">
     <a href="cv.php">
-        <button> ★ LET'S CREATE ★ </button>
+        <button> Je crée mon CV 🖋️ </button>
     </a>
 </div> 
 
